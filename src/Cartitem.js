e21489a85bc7af5876842ac2c81b1bf0015b9496
vipulkumarpandey111 <vipulkumarpandey111@gmail.com>
2021-06-10 00:44:37 +0530

    new commit

---
+++ src/Cartitem.js
@@ -0,0 +1,103 @@
+import React from 'react';
+
+class CartItem extends React.Component{
+    constructor(){
+        super();
+        this.state={
+            price:1999,
+            title:'Mobile Phone',
+            qty:1,
+            img:''
+        }
+        //this.testing();
+        //this.increaseQuantity=this.increaseQuantity.bind(this);
+    }
+    //outside of set state ,react doesn't perform batching
+    // in synchronous calls as promises or AJAX requests
+    // testing(){
+    //     const promise = new Promise((resolve,reject)=>{
+    //         setTimeout(()=>{
+    //             resolve('done');
+    //         },5000);
+    //     })
+    //     promise.then(()=>{
+    //         this.setState({qty:this.state.qty+10});
+
+    //         this.setState({qty:this.state.qty+10});
+
+    //         this.setState({qty:this.state.qty+10});
+    //         console.log('state',this.state);
+    //     });
+    // }
+    increaseQuantity = () => {
+        //console.log('this',this.state);
+        //setState form 1 - if previous state not required
+        // this.setState({
+        //     qty:this.state.qty+1
+        // });
+        //setState form 2 -if previous state required
+        this.setState((prevState)=>{
+            return{
+                qty:prevState.qty+1
+            }
+        });
+    }
+    decreaseQuantity = () => {
+        const {qty}=this.state;
+
+        if(qty==0){
+            return;
+        }
+        this.setState((prevState)=>{
+            return{
+                qty:prevState.qty-1
+            }
+        });
+    }
+    render(){
+        const {price,title,qty}=this.state;
+        return (
+            <div className="cart-item">
+                <div className="left-block">
+                    <img style={styles.image}/>
+                </div>
+                <div className="right-block">
+                    <div style={{fontSize:25}}>{title}</div>
+                    <div style={{color:'blue'}}>Rs {price}</div>
+                    <div style={{color:'blue'}}>Qty: {qty}</div>
+                    <div className="cart-item-actions">
+                    {/* Buttons */}
+                    <img 
+                    alt="increase" 
+                    className="action-icons" 
+                    src="https://image.flaticon.com/icons/png/128/992/992651.png"
+                     onClick={this.increaseQuantity}
+                     />
+                    <img 
+                    alt="decrease" 
+                    className="action-icons" 
+                    src="https://image.flaticon.com/icons/png/128/1828/1828906.png" 
+                    onClick={this.decreaseQuantity}
+                    />
+                    <img 
+                    alt="delete" 
+                    className="action-icons" 
+                    src="https://image.flaticon.com/icons/png/128/1345/1345874.png" 
+                    />
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+const styles = {
+    image:{
+        height:110,
+        width:110,
+        borderRadius:4,
+        background:'#ccc'
+    }
+}
+
+export default CartItem;
