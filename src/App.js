b8f9a0535eb18af8c5c91c3372d447e8e3e6eac8
vipulkumarpandey111 <vipulkumarpandey111@gmail.com>
2021-06-11 03:19:36 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,98 @@
 import React from 'react';
 import Cart from './Cart';
+import Navbar from './Navbar';
 
-function App() {
+class App extends React.Component {
+
+      constructor(){
+        super();
+        this.state={
+            products:[
+                {
+                    price:99,
+                    title:'Watch',
+                    qty:1,
+                    img:'https://images.unsplash.com/photo-1523170335258-f5ed11844a49?ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8d2F0Y2h8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60',
+                    id:1
+                },
+                {
+                    price:999,
+                    title:'Mobile Phone',
+                    qty:10,
+                    img:'https://images.unsplash.com/photo-1511707171634-5f897ff02aa9?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bW9iaWxlfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60',
+                    id:2
+                },
+                {
+                    price:9999,
+                    title:'Laptop',
+                    qty:5,
+                    img:'https://images.unsplash.com/photo-1587614382346-4ec70e388b28?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8bGFwdG9wfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60',
+                    id:3
+                }
+            ]
+        }
+    }
+    handleIncreaseQuantity = (product) => {
+        const {products}=this.state;
+        const index=products.indexOf(product);
+        products[index].qty+=1;
+        this.setState({
+            products:products
+        })
+    }
+    handleDecreaseQuantity = (product) => {
+        const {products}=this.state;
+        const index=products.indexOf(product);
+        if(products[index].qty==0){
+            return;
+        }
+        products[index].qty-=1;
+        this.setState({
+            products:products
+        })
+    }
+    handleDeleteProduct = (id) =>{
+        const {products} = this.state;
+        //to filter array of items and gives items with ids not equal to passed id
+        const items = products.filter((item)=>item.id!==id);
+        this.setState({
+            products:items
+        })
+
+    }
+  getCartCount = () =>{
+    const {products} = this.state;
+    let count  = 0;
+    products.forEach((product)=>{
+      count+=product.qty;
+    })
+    return count;
+  }
+  getCartTotal=()=>{
+    const {products} = this.state;
+    let cartTotal = 0;
+    products.map((product)=>{
+      cartTotal = cartTotal+product.qty*product.price
+    })
+    return cartTotal;
+  }
+  render(){
+    const {products} = this.state;
   return (
     //call CartItem in div
     <div className="App">
-        <Cart />
+        <Navbar count = {this.getCartCount()}/>
+        <Cart 
+         products={products}
+         onIncreaseQuantity = {this.handleIncreaseQuantity} 
+         onDecreaseQuantity = {this.handleDecreaseQuantity} 
+         onDeleteProduct = {this.handleDeleteProduct}
+         
+        />
+        <div style={{padding:10,fontSize:20,color:'grey'}}>TOTAL : {this.getCartTotal()}</div>
     </div>
   );
+  }
 }
 
 export default App;
--- src/Cartitem.js
+++ src/Cartitem.js
@@ -1,17 +1,18 @@
 import React from 'react';
 
-class CartItem extends React.Component{
-    constructor(){
-        super();
-        this.state={
-            price:1999,
-            title:'Mobile Phone',
-            qty:1,
-            img:''
-        }
-        //this.testing();
-        //this.increaseQuantity=this.increaseQuantity.bind(this);
-    }
+const CartItem = (props) => {
+//class CartItem extends React.Component{
+    // constructor(){
+    //     super();
+    //     this.state={
+    //         price:1999,
+    //         title:'Mobile Phone',
+    //         qty:1,
+    //         img:''
+    //     }
+    //     //this.testing();
+    //     //this.increaseQuantity=this.increaseQuantity.bind(this);
+    // }
     //outside of set state ,react doesn't perform batching
     // in synchronous calls as promises or AJAX requests
     // testing(){
@@ -29,37 +30,42 @@ class CartItem extends React.Component{
     //         console.log('state',this.state);
     //     });
     // }
-    increaseQuantity = () => {
-        //console.log('this',this.state);
-        //setState form 1 - if previous state not required
-        // this.setState({
-        //     qty:this.state.qty+1
-        // });
-        //setState form 2 -if previous state required
-        this.setState((prevState)=>{
-            return{
-                qty:prevState.qty+1
-            }
-        });
-    }
-    decreaseQuantity = () => {
-        const {qty}=this.state;
+    // increaseQuantity = () => {
+    //     //console.log('this',this.state);
+    //     //setState form 1 - if previous state not required
+    //     // this.setState({
+    //     //     qty:this.state.qty+1
+    //     // });
+    //     //setState form 2 -if previous state required
+    //     this.setState((prevState)=>{
+    //         return{
+    //             qty:prevState.qty+1
+    //         }
+    //     });
+    // }
+    // decreaseQuantity = () => {
+    //     const {qty}=this.state;
 
-        if(qty==0){
-            return;
-        }
-        this.setState((prevState)=>{
-            return{
-                qty:prevState.qty-1
-            }
-        });
-    }
-    render(){
-        const {price,title,qty}=this.state;
+    //     if(qty==0){
+    //         return;
+    //     }
+    //     this.setState((prevState)=>{
+    //         return{
+    //             qty:prevState.qty-1
+    //         }
+    //     });
+    // }
+   // render(){
+        const {price,title,qty}=props.product;
+        const {
+            product,
+            onIncreaseQuantity,
+            onDecreaseQuantity,
+            onDeleteProduct}=props;
         return (
             <div className="cart-item">
                 <div className="left-block">
-                    <img style={styles.image}/>
+                    <img style={styles.image} src={product.img}/>
                 </div>
                 <div className="right-block">
                     <div style={{fontSize:25}}>{title}</div>
@@ -71,24 +77,25 @@ class CartItem extends React.Component{
                     alt="increase" 
                     className="action-icons" 
                     src="https://image.flaticon.com/icons/png/128/992/992651.png"
-                     onClick={this.increaseQuantity}
+                     onClick={()=>onIncreaseQuantity(product)}
                      />
                     <img 
                     alt="decrease" 
                     className="action-icons" 
                     src="https://image.flaticon.com/icons/png/128/1828/1828906.png" 
-                    onClick={this.decreaseQuantity}
+                    onClick={()=>onDecreaseQuantity(product)}
                     />
                     <img 
                     alt="delete" 
                     className="action-icons" 
                     src="https://image.flaticon.com/icons/png/128/1345/1345874.png" 
+                    onClick={()=>onDeleteProduct(product.id)}
                     />
                     </div>
                 </div>
             </div>
         );
-    }
+   // }
 }
 
 const styles = {
